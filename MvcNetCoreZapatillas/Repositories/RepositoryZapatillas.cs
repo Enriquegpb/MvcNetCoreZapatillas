cfdb7a28ca27ddbdb4d2a60c7eb210c825ba8c59
serraguti <pacoserranox@gmail.com>
2023-03-16 11:51:15 +0100

    Add project files.

---
+++ MvcNetCoreZapatillas/Repositories/RepositoryZapatillas.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using MvcNetCoreZapatillas.Data;
+using MvcNetCoreZapatillas.Models;
+
+#region SQL SERVER
+//VUESTRO PROCEDIMIENTO DE PAGINACION DE IMAGENES DE ZAPATILLAS
+#endregion
+
+namespace MvcNetCoreZapatillas.Repositories
+{
+    public class RepositoryZapatillas
+    {
+        private ZapatillasContext context;
+
+        public RepositoryZapatillas(ZapatillasContext context)
+        {
+            this.context = context;
+        }
+    }
+}
